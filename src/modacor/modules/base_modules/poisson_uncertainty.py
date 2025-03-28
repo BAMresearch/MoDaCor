dfd97bfdb1c6ad03ed808c36c5205e8c7f413f95
toqduj <brian.pauw@bam.de>
2025-03-28 14:04:30 +0100

    something like this

--- src/modacor/modules/base_modules/poisson_uncertainty.py
+++ src/modacor/modules/base_modules/poisson_uncertainty.py
@@ -2,13 +2,35 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import annotations
-from modacor.dataclasses import ProcessStepExecutor, BaseData
+from pathlib import Path
+from modacor.dataclasses import ProcessStepExecutor, BaseData, ProcessStepDescriber
 import dask
 from modacor import ureg
 import dask.array as da
 
 
 class PoissonUncertainty(ProcessStepExecutor):
+    """
+    A class to add Poisson uncertainty to a signal.
+    This class is designed to be used as a processing step in the Modacor framework.
+    """
+    documentation = None
+
+    def __attrs_post_init__(self):
+        super().__attrs_post_init__()
+        self.documentation = ProcessStepDescriber(
+            calling_name = "Poisson Uncertainty estimator",
+            calling_id   = "PoissonUncertainty",
+            calling_module_path = Path("src/modacor/modules/base_modules/poisson_uncertainty.py"),
+            calling_version = "0.1",
+            required_data_keys=["Signal"],
+            calling_arguments=['Signal'],
+            step_keywords=['uncertainty', 'poisson', 'error', 'estimation', 'counting statistics'],
+            step_doc="Adds the Poisson uncertainty on data if the internal_data is in units of counts",
+            step_reference="DOI 10.1088/0953-8984/25/38/383201",
+            step_note="This is a simple Poisson uncertainty calculator, the uncertainty of a measurement cannot be lower than this",
+        )
+
     def can_apply(self) -> bool:
         """
         Check if the process can be applied to the given data.
