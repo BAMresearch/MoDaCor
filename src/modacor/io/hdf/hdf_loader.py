1fa7fd0ce72ff64851cf72625adb20045e34e575
Tim Snow <tim.snow@diamond.ac.uk>
2025-05-21 12:40:34 +0200

    Modified HDF Loader
    Will now look through the file to work out what's in the file and what shapes these datasets are. This may not be 100% useful in the future...

--- src/modacor/io/hdf/hdf_loader.py
+++ src/modacor/io/hdf/hdf_loader.py
@@ -30,6 +30,8 @@ __status__ = "Alpha"
 from ..io_source import IoSource
 from ..io_sources import IoSources
 from modacor.dataclasses.messagehandler import *
+
+from os.path import abspath
 from logging import WARNING
 import numpy as np
 import h5py
@@ -39,6 +41,10 @@ class HDFLoader(IoSources):
     def __init__(self, source_reference: str, source: IoSource, logging_level = WARNING):
         super().__init__(source_reference, source)
         self.hdf_logger = MessageHandler('hdf5logger', logging_level)
+        self._file_path = None
+        self._file_reference = None
+        self._file_datasets = []
+        self._file_datasets_shapes = {}
 
 
     def _open_file(self, file_path = None):
@@ -48,7 +54,9 @@ class HDFLoader(IoSources):
             raise OSError(error)
         
         try:
-            self.file_reference = h5py.File(file_path, 'r')
+            self._file_reference = h5py.File(file_path, 'r')
+            self._file_path = abspath(file_path)
+            self._file_reference.visititems(self._find_datasets)
         except OSError as error:
             self.hdf_logger.log.error(error)
             raise OSError(error)    
@@ -57,6 +65,18 @@ class HDFLoader(IoSources):
     def _close_file(self):
         try:
             self.file_reference.close()
+            self._file_path = None
+            self._file_reference = None
+            self._file_datasets.clear()
+            self._file_datasets_shapes.clear()
         except OSError as error:
             self.hdf_logger.log.error(error)
             raise OSError(error)
+
+
+    def _find_datasets(self, path_name, path_object):
+        """An internal function to be used to walk the tree of an HDF5 file and return a list of the datasets within"""
+
+        if(isinstance(self._file_reference[path_name], h5py._hl.dataset.Dataset)):
+            self._file_datasets.append(path_name)
+            self._file_datasets_shapes[path_name] = self._file_reference[path_name].shape
