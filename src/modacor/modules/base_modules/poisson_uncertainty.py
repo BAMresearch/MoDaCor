0de2352ad4f8e2e26db1099bf0aeed28c52675a9
Jerome Kieffer <jerome.kieffer@esrf.fr>
2025-05-21 08:20:30 +0200

    fix renaming of ProcessStep

--- src/modacor/modules/base_modules/poisson_uncertainty.py
+++ src/modacor/modules/base_modules/poisson_uncertainty.py
@@ -2,33 +2,39 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import annotations
+
 from pathlib import Path
 
 import pint
-from modacor.dataclasses import ProcessStepExecutor, ProcessStepDescriber
-# import dask
-# import dask.array as da
-from modacor.dataclasses.validators import check_data_element_and_units
+
+# import numpy as np
+from ...dataclasses.process_step import ProcessStep
+from ...dataclasses.process_step_describer import ProcessStepDescriber
+from ...dataclasses.validators import check_data_element_and_units
 
 
-class PoissonUncertainty(ProcessStepExecutor):
+class PoissonUncertainty(ProcessStep):
     """
     A class to add Poisson uncertainty to a signal.
     This class is designed to be used as a processing step in the Modacor framework.
     """
+
     documentation = ProcessStepDescriber(
-            calling_name="Poisson Uncertainty estimator",
-            calling_id="PoissonUncertainty",
-            calling_module_path=Path("src/modacor/modules/base_modules/poisson_uncertainty.py"),
-            calling_version="0.1",
-            required_data_keys=["Signal"],
-            works_on={'Signal': ['internal_data', 'uncertainties']},
-            step_keywords=['uncertainty', 'poisson', 'error', 'estimation', 'counting statistics'],
-            step_doc="Adds the Poisson uncertainty on data if the internal_data is in units of counts",
-            step_reference="DOI 10.1088/0953-8984/25/38/383201",
-            step_note="This is a simple Poisson uncertainty calculator, the uncertainty of a measurement cannot be lower than this",
-        )
-    
+        calling_name="Poisson Uncertainty estimator",
+        calling_id="PoissonUncertainty",
+        calling_module_path=Path("src/modacor/modules/base_modules/poisson_uncertainty.py"),
+        calling_version="0.1",
+        required_data_keys=["Signal"],
+        works_on={"Signal": ["internal_data", "uncertainties"]},
+        step_keywords=["uncertainty", "poisson", "error", "estimation", "counting statistics"],
+        step_doc="Adds the Poisson uncertainty on data if the internal_data is in units of counts",
+        step_reference="DOI 10.1088/0953-8984/25/38/383201",
+        step_note=(
+            "This is a simple Poisson uncertainty calculator, the uncertainty of a measurement"
+            " cannot be lower than this"
+        ),
+    )
+
     def __attrs_post_init__(self):
         super().__attrs_post_init__(self)
         self.documentation.calling_arguments = self.kwargs
@@ -37,7 +43,9 @@ class PoissonUncertainty(ProcessStepExecutor):
         """
         Check if the process can be applied to the given data.
         """
-        return check_data_element_and_units(self.data, "Signal", pint.Unit("counts"), self.message_handler)
+        return check_data_element_and_units(
+            self.data, "Signal", pint.Unit("counts"), self.message_handler
+        )
 
     def apply(self, **kwargs):
         # intensity_object: BaseData = self.kwargs["Signal"]
@@ -48,5 +56,5 @@ class PoissonUncertainty(ProcessStepExecutor):
         #     )
         # ]
         self.data.data["Signal"].uncertainties_origins += ["PoissonUncertainty"]
-        self.data.provenance += [self.documentation] # should be enough to recreate?
+        self.data.provenance += [self.documentation]  # should be enough to recreate?
         # self.stop()
