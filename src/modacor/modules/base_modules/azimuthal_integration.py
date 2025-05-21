125dacdc1b3fa37e65f9c8ec6364fee380204af5
Jerome Kieffer <jerome.kieffer@esrf.fr>
2025-05-21 17:07:30 +0200

    work on integration

--- src/modacor/modules/base_modules/azimuthal_integration.py
+++ src/modacor/modules/base_modules/azimuthal_integration.py
@@ -5,10 +5,12 @@ __copyright__ = "MoDaCor team"
 __license__ = "BSD3"
 __date__ = "21/05/2025"
 
-
+import warnings
 from pathlib import Path
 
-# import numpy as np
+import numpy as np
+
+from ...dataclasses.integrated_data import IntegratedData
 from ...dataclasses.process_step import ProcessStep
 from ...dataclasses.process_step_describer import ProcessStepDescriber
 from ...dataclasses.validators import check_data
@@ -27,8 +29,8 @@ class AzimuthalIntegration(ProcessStep):
         calling_id="AzimuthalIntegration",
         calling_module_path=Path(__file__),
         calling_version="0.0.1",
-        required_data_keys=["Signal"],
-        works_on={"Signal": ["raw_data", "variances", "normalization"]},
+        required_data_keys=["signal"],
+        works_on={"signal": ["raw_data", "variances", "normalization", "normalization_factor"]},
         step_keywords=["average"],
         step_doc="Add azimuthal integration date with variance propagated",
         step_reference="DOI 10.1107/S1600576724011038",
@@ -45,9 +47,27 @@ class AzimuthalIntegration(ProcessStep):
         """
         Check if the process can be applied to the given data.
         """
-        return check_data(self.data, "Signal", None, self.message_handler)
+        return check_data(self.bundle, "Signal", None, self.message_handler)
 
-    def apply(self, **kwargs):
-        self.data.data["sum_signal"] = self.sparse
-        self.data.provenance += [self.documentation]  # should be enough to recreate?
-        # self.stop()
+    def apply(self, apply_scalers, **kwargs):
+        source = self.bundle["signal"]
+        signal = source.signal
+        normalization = source.normalization
+        integrated = self.bundle["integrated"] = IntegratedData(
+            sum_signal=self.sparse.dot(signal),
+            sum_normalization=self.sparse.dot(normalization),
+            sum_normalization_squared=self.sparse_squared.dot(normalization * normalization),
+            normalization_factor=source.normalization_factor,
+            normalization_factor_variance=source.normalization_factor_variance,
+            sem={},
+            std={},
+        )
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            integrated.mean = integrated.sum_signal / integrated.sum_normalization
+            for key, var in source.variances.items():
+                integrated.sum_variance[key] = self.sparse_squared.dot(var)
+                integrated.std = np.sqrt(integrated.sum_signal) / integrated.sum_normalization
+                integrated.sem = np.sqrt(
+                    integrated.sum_signal / integrated.sum_normalization_squared
+                )
