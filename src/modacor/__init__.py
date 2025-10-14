019a7e4f700d865834df0165a436b285b2a9c27a
toqduj <brian.pauw@bam.de>
2025-10-14 19:00:39 +0200

    define an arbitrary intensity unit

--- src/modacor/__init__.py
+++ src/modacor/__init__.py
@@ -6,6 +6,8 @@ __version__ = "1.0.0"
 from pint import UnitRegistry, set_application_registry
 
 ureg = UnitRegistry(system="SI")
+# we need to define an arbitrary intensity unit for scaling of intensity data:
+ureg.define("AIU = [intensity] = arbitrary_intensity_unit")
 Q_ = ureg.Quantity
 # recommended for pickling and unpickling:
 set_application_registry(ureg)
