62e542fc3d0a274ce94dee590711b5207fbd2b41
toqduj <brian.pauw@bam.de>
2025-12-16 16:38:29 +0100

    fixing unit naming to make it clearer

--- src/modacor/__init__.py
+++ src/modacor/__init__.py
@@ -7,7 +7,7 @@ from pint import UnitRegistry, set_application_registry
 
 ureg = UnitRegistry(system="SI")
 # we need to define an arbitrary intensity unit for scaling of intensity data:
-ureg.define("AIU = [intensity] = arbitrary_intensity_unit")
+ureg.define("AFU = [flux] = arbitrary_flux_unit")
 Q_ = ureg.Quantity
 # recommended for pickling and unpickling:
 set_application_registry(ureg)
