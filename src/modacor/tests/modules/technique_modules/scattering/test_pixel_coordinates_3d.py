a6e3710fc5153e6f9fcad4c729d263d2a7739391
toqduj <brian.pauw@bam.de>
2026-01-04 17:48:29 +0100

    cleanup

--- src/modacor/tests/modules/technique_modules/scattering/test_pixel_coordinates_3d.py
+++ src/modacor/tests/modules/technique_modules/scattering/test_pixel_coordinates_3d.py
@@ -19,7 +19,7 @@ from modacor.dataclasses.basedata import BaseData
 from modacor.dataclasses.databundle import DataBundle
 from modacor.dataclasses.processing_data import ProcessingData
 from modacor.io.io_sources import IoSources
-from modacor.modules.technique_modules.scattering.pixel_coordinates_3d_template import (
+from modacor.modules.technique_modules.scattering.pixel_coordinates_3d import (
     CanonicalDetectorFrame,
     PixelCoordinates3DTemplate,
 )
