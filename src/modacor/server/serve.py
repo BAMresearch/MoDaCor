44a04acea96fb29da7c96941e9956c86b4677dab
Ingo Breßler <dev@ingobressler.net>
2026-04-28 15:03:42 +0200

    style(server): fix whitespace formatting

--- src/modacor/server/serve.py
+++ src/modacor/server/serve.py
@@ -5,8 +5,10 @@
 from modacor.server.api import create_app
 from modacor.server.session_manager import SessionManager
 
+
 def main():
     return create_app(SessionManager())
+
 
 if __name__ == "__main__":
     raise SystemExit(main())
